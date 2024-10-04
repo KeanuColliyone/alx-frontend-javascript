52d2a95492e85fd78e95108dd22157c8e26fdd6b
Kenan Kotzee <frostcolliyone@gmail.com>
2024-10-04 19:04:21 +0000

    0x02-ES6_classes

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -2,13 +2,7 @@ import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
-    // Call the parent class constructor
     super(sqft);
-
-    // Validate floors and assign it to _floors
-    if (typeof floors !== 'number') {
-      throw new TypeError('Floors must be a number');
-    }
     this._floors = floors;
   }
 
@@ -17,8 +11,8 @@ export default class SkyHighBuilding extends Building {
     return this._floors;
   }
 
-  // Override the evacuationWarningMessage method
+  // Override evacuationWarningMessage method
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
