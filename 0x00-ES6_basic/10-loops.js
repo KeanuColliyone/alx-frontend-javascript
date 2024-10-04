958cc13a341660a751b17b79ffe30cfc6cae5eff
Kenan Kotzee <frostcolliyone@gmail.com>
2024-10-04 13:12:08 +0000

    0x00-ES6_basic

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-    for (let value of array) {
-      array[array.indexOf(value)] = appendString + value;
-    }
-  
-    return array;
-  }  
+  const newArray = [];
+
+  for (const value of array) {
+    newArray.push(appendString + value);
+  }
+
+  return newArray;
+}
