7fd934eb25dd7362691810b65ead4146ee3757ee
Kenan Kotzee <frostcolliyone@gmail.com>
2024-10-04 19:11:29 +0000

    0x02-ES6_classes

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,10 +1,11 @@
-// HolbertonClass definition
+// Define and export the HolbertonClass class
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
     this._location = location;
   }
 
+  // Getters
   get year() {
     return this._year;
   }
@@ -14,37 +15,39 @@ export class HolbertonClass {
   }
 }
 
-// StudentHolberton definition
+// Define and export the StudentHolberton class
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
-    this._holbertonClass = holbertonClass; // Assign holbertonClass properly
+    this._holbertonClass = holbertonClass; // Assign holbertonClass to the instance
   }
 
+  // Getters
   get fullName() {
     return `${this._firstName} ${this._lastName}`;
   }
 
   get holbertonClass() {
-    return this._holbertonClass; // Fix getter
+    return this._holbertonClass; // Return the class instance
   }
 
   get fullStudentDescription() {
+    // Correct usage of this to refer to instance properties
     return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
 
-// Create HolbertonClass instances
+// Create instances of HolbertonClass
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-// Create StudentHolberton instances
+// Create instances of StudentHolberton with references to their class instances
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-// Export list of students
+// Export the list of students
 export const listOfStudents = [student1, student2, student3, student4, student5];
